a280fbdee376ba5ea565d879cc08f7cd0cc73aae
amalalreshi <amal.alreshi@gmail.com>
2024-05-20 10:21:07 +0300

    Initial Commit

---
+++ Q-Lnk/Q_LnkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q_LnkApp.swift
+//  Q-Lnk
+//
+//  Created by Amal Mohammad Alreshi on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Q_LnkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
